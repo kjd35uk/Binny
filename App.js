d25841195e134a4914ee194ffbef9a44530f4037
Kirstie Davidson <kirstie.davidson@thebeansgroup.com>
2021-02-07 20:22:12 +0000

    Add working binny

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { findDOMNode } from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
+import * as data from './data.json';
 
 export default function App() {
+
+  const today = new Date().toDateString()
+
+function isAfterToday(date) {
+  return Date.parse(Object.keys(date)) >= new Date()
+}
+
+const binDay = Object.values(data.dates.find(isAfterToday)).toString()
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Magical Binny!</Text>
+      <Text>Today's date is {today}</Text>
+      <Text>The next bin day is: {binDay}</Text>
       <StatusBar style="auto" />
     </View>
   );
